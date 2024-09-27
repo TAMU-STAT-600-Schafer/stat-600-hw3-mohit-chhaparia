339bd65d74a2941bfdd519e95538e42b0e682e2e
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-09-26 19:06:21 -0500

    Added code to add intercept column to X and Xt and a call for the LRMultiClass function with lambda = 1 and 50 iterations using the letter-test data.

--- Letter_example.R
+++ Letter_example.R
@@ -13,12 +13,14 @@ Yt <- letter_test[, 1]
 Xt <- as.matrix(letter_test[, -1])
 
 # [ToDo] Make sure to add column for an intercept to X and Xt
+X <- cbind(rep(1, nrow(X)), X)
+Xt <- cbind(rep(1, nrow(Xt)), Xt)
 
 # Source the LR function
 source("FunctionsLR.R")
 
 # [ToDo] Try the algorithm LRMultiClass with lambda = 1 and 50 iterations. Call the resulting object out, i.e. out <- LRMultiClass(...)
-
+out <- LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
 
 # The code below will draw pictures of objective function, as well as train/test error over the iterations
 plot(out$objective, type = 'o')
