6d9dfc2ba079a44335e977185e5b0625ad650376
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-09-27 18:52:14 -0500

    Created a sample data set and function call for testing.

--- Tests.R
+++ Tests.R
@@ -1,2 +1,23 @@
 # This is a script to save your own tests for the function
 source("FunctionsLR.R")
+
+set.seed(0928)
+
+# Dataset for testing
+n <- 100
+p <- 3
+K <- 3
+
+# Normal X and Xt populations
+X <- cbind(1, matrix(rnorm(n * (p - 1)), n, p - 1))
+Xt <- cbind(1, matrix(rnorm(n * (p - 1)), n, p - 1))
+
+# Random class labels
+y <- sample(0:(K - 1), n, replace = TRUE)
+yt <- sample(0:(K - 1), n, replace = TRUE)
+
+# Initialize beta_init
+beta_init <- matrix(rep(0, p * K), p, K)
+
+# Function call
+out <- LRMultiClass(X, y, Xt, yt, 50, 0.1, 1, beta_init)
